05994c6ed02ca4389d660df44d2d53b929f94d49
s.ciotti <s.ciotti@jsitaly.it>
2019-05-04 20:32:17 +0200

    Python practice

--- MyFirstFl30.py
+++ MyFirstFl30.py
@@ -35,3 +35,31 @@ print(evenMoreStates)
 
 #------------------------------------
 
+exList = ["pass", "this", "list", "to", "a", "function"]
+def list_passer(li):
+    return li
+
+print(list_passer(exList))
+print(list_passer([1, 2, 3]))
+
+exList = [9, 0, 2, 1, 0]
+def list_accessor(li):
+    return li[3]
+print(list_accessor(exList))
+
+exList = [9, 0, 2, 1, 0]
+def list_modifier(li):
+    return li[2] / 0.5
+print(list_modifier(exList))
+
+exList = [1, 1, 3, 8]
+
+def list_modifier(li):
+    li.remove(3)
+    li.insert(2, 4)
+    return li
+
+# prints [1, 1, 4, 8]
+print(list_modifier(exList))
+
+#------------------------------------
